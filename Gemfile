f800c5cb77a16acc8aecd373e4a10bbd2e3bba13
akakitani <atsunori.kakitani@gmail.com>
2015-04-24 18:50:46 -0700

    Goodbye bourbon/neat hello bootstrap

--- Gemfile
+++ Gemfile
@@ -3,9 +3,9 @@ source 'https://rubygems.org'
 gem 'github-pages', '~> 34'
 gem 'jekyll-assets', '~> 0.14.0'
 
-gem 'bourbon', '~> 4.2.2'
-gem 'neat', '~> 1.7.2'
 gem 'font-awesome-sass', '~> 4.3.2.1'
 
 gem 'sass', '~> 3.4.13'
 gem 'uglifier', '~> 2.7.1'
+
+gem 'bootstrap-sass', '~> 3.3.4'
--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,4 +1,7 @@
 require "jekyll-assets"
-require "jekyll-assets/bourbon"
-require "jekyll-assets/neat"
 require "jekyll-assets/font-awesome"
+
+# bootstrap requires minimum precision of 10, see https://github.com/twbs/bootstrap-sass/issues/409
+::Sass::Script::Number.precision = [10, ::Sass::Script::Number.precision].max
+
+require "jekyll-assets/bootstrap"
